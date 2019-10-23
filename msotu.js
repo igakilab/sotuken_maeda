ff8cce5de03d041921627feffc66a7898c491fca
Unknown <taketatukousho81288@gmail.com>
2019-10-23 18:16:02 +0900

    ストーリーを選択する機能追加
    クリックしたストーリーを選択するという機能を追加した
    ・選択されてないストーリーは移動ができない
    ・ストーリーを選択していないとサイコロが触れない

--- msotu.js
+++ msotu.js
@@ -1,31 +1,25 @@
-let clik = 0;
-let daiceme = 0;
-let t1 = 24;
-let t2 = 21;
-let t3 = 27;
-let f1 = 0;
-let f2 = 0;
-let log = [];
-let cf = 0;
-let pb = 0;
-
+let clik = 0; //クリック総数
+let daiceme = 0; //ダイスの目
+let t1 = 24; //ストーリー1のタスク
+let t2 = 21; //ストーリー2のタスク
+let t3 = 27; //ストーリー3のタスク
+let cf = 0; //チャンスカードを引いているか判断
+let select = 0;//選択されているストーリー
+//ダイスの画像とチャンスカードの画像をデフォルトに戻す"
 function reset() {
   document.images['dice'].src = '6d.gif'
   document.images['dice1'].src = '6d.gif'
   document.images['card'].src = "card.png"
 }
 
-function ploblem(){
-  if(pb==1){
-    document.images['task1'].src = 't1.jpg'
-  }
-}
 
-/乱数で１～６の数字を生成しdaice.numに代入。数字に合ったgifを再生/
-
+//乱数で１～６の数字を生成しdaice.numに代入。数字に合ったgifを再生
 function daice(e) {
-  if (daiceme == 0) {
+  if(select == 0){
+    window.alert('storyを選択してください');
+  }else if (daiceme == 0) {
     clikc();
+    ring();
     let daice = Math.floor(Math.random() * 6) + 1;
     document.images['dice'].src = '6d_0' + daice + '.gif'
 
@@ -35,20 +29,25 @@ function daice(e) {
   }
 }
 
-/daicesoundを再生する/
 
+//daicesoundを再生する
 function ring() {
   document.getElementById("daicesound").play();
 }
 
-/ダイスを回した回数を数える/
 
+function sound() {
+  document.getElementById("clikcsound").play();
+}
+
+
+//ダイスを回した回数を数える
 function clikc() {
   clik = clik + 1;
 }
 
-/タスク1をクリックしたときの処理/
 
+//タスク1をクリックしたときの処理
 function disp1(num) {
   if (num == 1) {
     if (t1 <= 0) {
@@ -56,8 +55,9 @@ function disp1(num) {
     } else if (t1 < 24) {
       window.alert('task1はDoingです.Doingに移動させてください');
     } else if (t1 > 0) {
-        document.getElementById( "task" ).innerHTML = t1;
-      }
+      document.getElementById("task").innerHTML = t1;
+      select = 1;
+    }
   }
 
   if (num == 2) {
@@ -68,12 +68,14 @@ function disp1(num) {
       daiceme = 0;
       cf = 0;
       reset();
+      sound();
       if (t1 > 0) {
-        document.getElementById( "task" ).innerHTML = t1
+        document.getElementById("task").innerHTML = t1
+        select = 1;
       } else {
         window.alert('お疲れ様です.task1をDoneに移動しましょう');
-        document.getElementById( "task" ).innerHTML =0;
-        f1 = 1;
+        document.getElementById("task").innerHTML = 0;
+        select = 0;
       }
     }
   }
@@ -90,93 +92,99 @@ function disp1(num) {
 }
 
 
-/タスク2をクリックしたときの処理/
-
+//タスク2をクリックしたときの処理
 function disp2(num, be) {
-      if (num == 1) {
-        if (t2 <= 0) {
-          window.alert('task2はDoneです.Doneに移動させてください');
-        } else if (t2 < 21) {
-          window.alert('task2はDoingです.Doingに移動させてください');
-        } else if (t2 > 0) {
-            document.getElementById( "task" ).innerHTML = t2;
-          }
-      }
+  if (num == 1) {
+    if (t2 <= 0) {
+      window.alert('task2はDoneです.Doneに移動させてください');
+    } else if (t2 < 21) {
+      window.alert('task2はDoingです.Doingに移動させてください');
+    } else if (t2 > 0) {
+      document.getElementById("task").innerHTML = t2;
+      select = 2;
+    }
+  }
 
-      if (num == 2) {
-        if (t2 <= 0) {
-          window.alert('task2はDoneです.Doneに移動させてください');
-        } else {
-          t2 = t2 - daiceme;
-          daiceme = 0;
-          cf = 0;
-          reset();
-          if (t2 > 0) {
-            document.getElementById( "task" ).innerHTML = t2;
-          } else {
-            window.alert('お疲れ様です.task2をDoneに移動しましょう');
-            document.getElementById( "task" ).innerHTML = 0;
-            f2 = 1;
-          }
-        }
+  if (num == 2) {
+    if (t2 <= 0) {
+      window.alert('task2はDoneです.Doneに移動させてください');
+    } else {
+      t2 = t2 - daiceme;
+      daiceme = 0;
+      cf = 0;
+      reset();
+      sound();
+      if (t2 > 0) {
+        document.getElementById("task").innerHTML = t2;
+        select = 2;
+      } else {
+        window.alert('お疲れ様です.task2をDoneに移動しましょう');
+        document.getElementById("task").innerHTML = 0;
+        select = 0;
       }
+    }
+  }
 
-      if (num == 3) {
-        if (t2 <= 0) {
-          window.alert('task2はDoneです');
-        } else if (t2 < 21) {
-          window.alert('task2はDoingです.Doingに移動させてください');
-        } else {
-          window.alert('task2はToDoです.ToDoに移動させてください');
-        }
-      }
+  if (num == 3) {
+    if (t2 <= 0) {
+      window.alert('task2はDoneです');
+    } else if (t2 < 21) {
+      window.alert('task2はDoingです.Doingに移動させてください');
+    } else {
+      window.alert('task2はToDoです.ToDoに移動させてください');
+    }
+  }
 }
 
-/タスク3をクリックしたときの処理/
 
+//タスク3をクリックしたときの処理
 function disp3(num, be) {
-      if (num == 1) {
-        if (t3 <= 0) {
-          window.alert('task3はDoneです.Doneに移動してもう一度クリックしてください');
-        } else if (t3 < 27) {
-          window.alert('task3はDoingです.Doingに移動させてください');
-        } else {
-          if (t3 > 0) {
-            document.getElementById( "task" ).innerHTML = t3;
-          }
-        }
+  if (num == 1) {
+    if (t3 <= 0) {
+      window.alert('task3はDoneです.Doneに移動してもう一度クリックしてください');
+    } else if (t3 < 27) {
+      window.alert('task3はDoingです.Doingに移動させてください');
+    } else {
+      if (t3 > 0) {
+        document.getElementById("task").innerHTML = t3;
+        select = 3;
       }
+    }
+  }
 
-      if (num == 2) {
-        if (t3 <= 0) {
-          window.alert('task3はDoneです.Doneに移動してもう一度クリックしてください');
-        } else {
-          t3 = t3 - daiceme;
-          daiceme = 0;
-          cf = 0;
-          reset();
-          if (t3 > 0) {
-            document.getElementById( "task" ).innerHTML = t3;
-          } else {
-            window.alert('お疲れ様です.task3をDoneに移動してもう一度クリックしてください');
-            document.getElementById( "task" ).innerHTML = 0;
-          }
-        }
+  if (num == 2) {
+    if (t3 <= 0) {
+      window.alert('task3はDoneです.Doneに移動してもう一度クリックしてください');
+    } else {
+      t3 = t3 - daiceme;
+      daiceme = 0;
+      cf = 0;
+      reset();
+      sound();
+      if (t3 > 0) {
+        document.getElementById("task").innerHTML = t3;
+        select = 3;
+      } else {
+        window.alert('お疲れ様です.task3をDoneに移動してもう一度クリックしてください');
+        document.getElementById("task").innerHTML = 0;
+        select = 0;
       }
+    }
+  }
 
-      if (num == 3) {
-        if (t3 <= 0) {
-          window.alert("クリアおめでとう！！ スコアは" + clik + "回です");
-        } else if (t3 < 27) {
-          window.alert('task3はDoingです.Doingに移動させてください');
-        } else {
-          window.alert('task3はToDoです.ToDoに移動させてください');
-        }
-      }
+  if (num == 3) {
+    if (t3 <= 0) {
+      window.alert("クリアおめでとう！！ スコアは" + clik + "回です");
+    } else if (t3 < 27) {
+      window.alert('task3はDoingです.Doingに移動させてください');
+    } else {
+      window.alert('task3はToDoです.ToDoに移動させてください');
+    }
+  }
 }
 
 
-
+//チャンスカードのクリックしたときの動き
 function chance() {
   if (daiceme == 0) {
     window.alert('先にダイスを振りましょう');
@@ -188,6 +196,7 @@ function chance() {
   }
 }
 
+//チャンスカードの処理
 function event(c) {
 
   if (c == 1) {
@@ -212,23 +221,23 @@ function event(c) {
     }
   }
 
- if(c==4){
-   daiceme = 1000;
- }
+  if (c == 4) {
+    daiceme = 1000;
+  }
 
- if(c==5){
-   daiceme= daiceme*2;
- }
+  if (c == 5) {
+    daiceme = daiceme * 2;
+  }
 
- if(c==6){
-   daiceme=daiceme + 4;
- }
+  if (c == 6) {
+    daiceme = daiceme + 4;
+  }
 
-if(c==7){
-  document.images['task1'].src = 't1.1.jpg'
-}
+  if (c == 7) {
+    document.images['task1'].src = 't1.1.jpg'
+  }
 
- if (c==8){
-  daiceme=daiceme/2;
- }
+  if (c == 8) {
+    daiceme = daiceme / 2;
+  }
 }
