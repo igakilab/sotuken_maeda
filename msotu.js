88f9fa7524edec43ba3fc1b194a0367a6cb1ecaa
takeshi <taketatukousho81288@gmail.com>
2020-01-09 18:53:15 +0900

    進捗記録表のコードをｍsotuに記述

--- msotu.js
+++ msotu.js
@@ -539,3 +539,45 @@ function sum(roubd) {
     log[round] += task[i];
   }
 }
+
+
+
+function example() {
+
+var tasks = [
+{"startDate":new Date("2015/01/01 "),"endDate":new Date("2015/01/02 "),"taskName":"story1","status":"RUNNING"},
+{"startDate":new Date("2015/01/02 "),"endDate":new Date("2015/01/03 "),"taskName":"story2","status":"RUNNING"},
+{"startDate":new Date("2015/01/03 "),"endDate":new Date("2015/01/04 "),"taskName":"story3","status":"RUNNING"},
+{"startDate":new Date("2015/01/04 "),"endDate":new Date("2015/01/05 "),"taskName":"story4","status":"RUNNING"},
+{"startDate":new Date("2015/01/05 "),"endDate":new Date("2015/01/06 "),"taskName":"story5","status":"RUNNING"},
+{"startDate":new Date("2015/01/06 "),"endDate":new Date("2015/01/07 "),"taskName":"story6","status":"RUNNING"},
+{"startDate":new Date("2015/01/07 "),"endDate":new Date("2015/01/08 "),"taskName":"story7","status":"RUNNING"},
+{"startDate":new Date("2015/01/08 "),"endDate":new Date("2015/01/09 "),"taskName":"story8","status":"RUNNING"},
+{"startDate":new Date("2015/01/09 "),"endDate":new Date("2015/01/10 "),"taskName":"story9","status":"RUNNING"},
+{"startDate":new Date("2015/01/10 "),"endDate":new Date("2015/01/11 "),"taskName":"story10","status":"RUNNING"},
+];
+
+var taskStatus = {
+    "SUCCEEDED" : "bar",
+    "FAILED" : "bar-failed",
+    "RUNNING" : "bar-running",
+    "KILLED" : "bar-killed"
+};
+
+var taskNames = [ "story1","story2","story3","story4","story5","story6","story7","story8","story9","story10" ];
+
+tasks.sort(function(a, b) {
+    return a.endDate - b.endDate;
+});
+var maxDate = tasks[tasks.length - 1].endDate;
+tasks.sort(function(a, b) {
+    return a.startDate - b.startDate;
+});
+var minDate = tasks[0].startDate;
+
+var format = "%d";
+
+var gantt = d3.gantt().taskTypes(taskNames).taskStatus(taskStatus).tickFormat(format);
+gantt(tasks);
+
+};
